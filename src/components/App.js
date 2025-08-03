d12653a1deb4a7dcec9c1814709c8baa0395f104
vikash984 <vikashk15072001@gmail.com>
2025-08-03 17:37:18 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import UserList from "./components/UserList";
-import UserDetails from "./components/UserDetails";
+import UserList from "./UserList";
+import UserDetails from "./UserDetails";
 import "../styles/App.css";
 
 const App = () => {
