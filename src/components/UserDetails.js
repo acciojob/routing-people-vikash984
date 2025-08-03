8e07b08971a8e4ff516db91316343a1af4fe7fec
vikash984 <vikashk15072001@gmail.com>
2025-08-03 17:26:38 +0530

    Create UserDetails.js

---
+++ src/components/UserDetails.js
@@ -0,0 +1,33 @@
+
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+const UserDetails = () => {
+  const { id } = useParams();
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(true);
+    fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setUser(data);
+        setLoading(false);
+      });
+  }, [id]);
+
+  if (loading) return <div>Loading...</div>;
+
+  return (
+    <div>
+      <p>Name: {user.name}</p>
+      <p>Username: {user.username}</p>
+      <p>Email: {user.email}</p>
+      <p>Phone: {user.phone}</p>
+      <p>Website: {user.website}</p>
+    </div>
+  );
+};
+
+export default UserDetails;
